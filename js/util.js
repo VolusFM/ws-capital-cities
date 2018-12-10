acc517159df689d66ddc1889a23a76eec9b179c2
Rigolle Valentin <vrigolle@if501-208-02.insa-lyon.fr>
2018-12-10 09:00:45 +0100

    Lecture fichier requetes

--- js/util.js
+++ js/util.js
@@ -12,3 +12,9 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
+
+function readTextFile(filename, callback) {
+    $.get(filename, function(data) {
+        callback(data);
+    }, 'text');
+}
